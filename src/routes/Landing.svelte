e35a47de2198f1a67a9a683a4836c5dad7e217d8
Injng <jlinjiang@gmail.com>
2023-10-01 13:46:21 -0700

    Basic landing page

---
+++ src/routes/Landing.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css"
+
+    export let title, text, ref, next;
+</script>
+<div class="flex flex-col items-center justify-center h-screen bg-gray-200">
+    <h1 class="text-5xl font-bold mb-4" id="{ref}">{title}</h1>
+    <p class="text-xl text-center mb-8 px-4">
+        {text}
+    </p>
+    <div class="flex justify-center">
+        <a href="#{next}" class="text-6xl">&#x2193;</a>
+    </div>
+</div>
