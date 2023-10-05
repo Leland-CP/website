90e5222bed9804499b0c6c916fd4f13ab16c4799
Injng <jlinjiang@gmail.com>
2023-10-05 11:48:55 -0700

    Randomize game target

--- src/routes/progame/+page.svelte
+++ src/routes/progame/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../../app.css"
+
+    const targetX = Math.floor(Math.random() * 2000) + 1;
+    const targetY = Math.floor(Math.random() * 1000) + 1;
+
     let m = { x: 0, y: 0 };
     let state = true;
 
@@ -7,7 +11,7 @@
         m.x = event.clientX;
         m.y = event.clientY;
 
-        if (m.x === 1000 && m.y === 500) {
+        if (m.x === targetX && m.y === targetY) {
             state = false;
         } else {
             state = true;
@@ -17,6 +21,7 @@
 
 {#if state}
     <div on:pointermove={handleMove} class="w-100% h-screen p-8">
+        <span>Target: {targetX}, {targetY}</span>
         The pointer is at:
         <h1 class="text-3xl">x: {m.x}</h1>
         <h1 class="text-3xl">y: {m.y}</h1>
