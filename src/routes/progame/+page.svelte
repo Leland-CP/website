e700cdce2c3ef1f2a4c31ef7d259738f2871bc86
Injng <jlinjiang@gmail.com>
2023-10-06 10:18:11 -0700

    Add stopwatch to pro game

--- src/routes/progame/+page.svelte
+++ src/routes/progame/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import "../../app.css"
+    import {onMount} from "svelte";
 
     const targetX = Math.floor(Math.random() * 2000) + 1;
     const targetY = Math.floor(Math.random() * 1000) + 1;
 
     let m = { x: 0, y: 0 };
     let state = true;
+    let start = Date.now();
+    let timing = 0;
 
     function handleMove(event) {
         m.x = event.clientX;
@@ -17,14 +20,22 @@
             state = true;
         }
     }
+
+    function stopwatch() {
+        requestAnimationFrame(stopwatch);
+        timing = (Date.now() - start) / 1000;
+    }
+
+    onMount(stopwatch);
 </script>
 
 {#if state}
     <div on:pointermove={handleMove} class="w-100% h-screen p-8">
         <span>Target: {targetX}, {targetY}</span>
-        The pointer is at:
+        <span>The pointer is at:</span>
         <h1 class="text-3xl">x: {m.x}</h1>
         <h1 class="text-3xl">y: {m.y}</h1>
+        <h1 class="relative bottom-0 text-3xl">{timing}</h1>
     </div>
 {:else}
     <div class="w-100% h-screen p-8">
