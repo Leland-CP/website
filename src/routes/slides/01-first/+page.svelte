41fe173a974f118c52327800272490132118e896
lnjng <jlinjiang@gmail.com>
2023-10-19 00:05:51 -0700

    Add reveal.js support

---
+++ src/routes/slides/01-first/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { RevealJsContext, Slide } from 'svelte-reveal.js';
+
+    import 'reveal.js/dist/reset.css';
+    import 'reveal.js/dist/reveal.css';
+    import 'reveal.js/dist/theme/white.css';
+</script>
+
+<!-- a container with fixed size is required -->
+<div style:width="100%" style:height="100vh">
+    <RevealJsContext>
+        <Slide>
+            <h1>Hello world!</h1>
+        </Slide>
+    </RevealJsContext>
+</div>
